547a02f91fd9bfa1459d3f05ab4c37f43aecc3f1
dontBotherMyName <10bparth@gmail.com>
2020-06-07 23:17:25 +0530

    Module 4 complete

--- module4_solution/js/SpeakGoodBye.js
+++ module4_solution/js/SpeakGoodBye.js
@@ -1,7 +1,13 @@
-var speakWord = "Good Bye";
+(function(window) {
 
-var byeSpeaker = {};
+    var byeSpeaker = {};
 
-byeSpeaker.speak = function (name) {
-    console.log(speakWord + " " + name);
-}
+    var speakWord = "Good Bye";
+  
+    byeSpeaker.speak = function(name) {
+      console.log(speakWord + " " + name);
+    }
+  
+    window.byeSpeaker = byeSpeaker;
+  
+  })(window);
